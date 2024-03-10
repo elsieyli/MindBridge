eca8cdc1da09e899b0550d06140f0b4b425ed29a
elsieyli <elsie.snowflake1@gmail.com>
2024-03-10 03:58:11 -0400

    icon

--- mindbridgewebapp/src/components/SideBar.js
+++ mindbridgewebapp/src/components/SideBar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {NavLink, useLocation} from "react-router-dom";
-import HappyFace from "../assets/HappyFacecopy.png";
+import HappyFace from "../assets/happy.png";
 import Message from "../assets/message.png";
 
 import "../styles/SideBar.css";
