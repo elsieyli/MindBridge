20263a9bbf8369aaafd3d983fb8c90c057bdb584
Omar El-Sawy <omarel-sawy@Omars-MacBook-Pro.local>
2024-03-10 05:05:03 -0400

    Twilio eendpoint

--- backend/src/Endpoints.py
+++ backend/src/Endpoints.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI, Security
+from fastapi import FastAPI, Security, Request
 from src.application.util import VerifyToken
 from src.helper.MongoDB import DB
 from src.helper.Middleware import PreProcessMiddleware, TimerMiddleWare
 from src.helper.Twilio import SendMessage
+from twilio.twiml.messaging_response import MessagingResponse
 
 # Creates app instance
 app = FastAPI()
@@ -16,6 +17,23 @@ app.add_middleware(TimerMiddleWare)
 # Storing Information using Databases
 UserLogin = DB("Users", "Login")
 
+@app.post("/sms")
+async def forward_sms(request: Request):
+    # Parse the form data sent by Twilio
+    form_data = await request.form()
+    incoming_msg = form_data.get('Body')
+    from_number = form_data.get('From')
+    
+    # Specify the number you want to forward messages to
+    forward_to_number = "+1234567890"
+
+    # Create TwiML response to forward the message
+    response = MessagingResponse()
+    message = response.message(f"Forwarded from {from_number}: {incoming_msg}")
+    message.to(forward_to_number)
+    
+    # Return the TwiML
+    return Response(content=str(response), media_type="application/xml")
 
 # Send Message Endpoint with body <Known existing phone number and teacher phone numberr>
 @app.post("/Send/{body}")
