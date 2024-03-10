f1fbe517f4746ad45bca7216a26e008170448269
elsieyli <elsie.snowflake1@gmail.com>
2024-03-09 19:00:34 -0500

    Update Home.js

--- mindbridgewebapp/src/pages/Home.js
+++ mindbridgewebapp/src/pages/Home.js
@@ -2,9 +2,7 @@ import React from 'react';
 import NavBar from '../components/NavBar';
 import SideBar from '../components/SideBar';
 import Button from '../components/Button';
-import './home.css';
 
-// Import all the required icons
 import foodIcon from '../assets/FoodIcon.png';
 import waterIcon from '../assets/water.png';
 import outsideIcon from '../assets/outside.png';
