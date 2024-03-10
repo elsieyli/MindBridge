41da29933f0d8bafb3cfd7ce081c4d992ec9f814
Ryan Sadeghi-Javid <javidryan7@gmail.com>
2024-03-09 19:10:57 -0500

    button added

--- mindbridgewebapp/src/components/Button.js
+++ mindbridgewebapp/src/components/Button.js
@@ -1,3 +1,17 @@
-export default function Button({ children, ...props }) {
-  return <button {...props}>{children}</button>;
+import React from 'react'
+import "../styles/Button.css"
+
+function Button({ image, audio }) {
+  return (
+    <div
+      className="Button"
+      onClick={() => {
+        
+      }}
+    >
+    <div style={{ backgroundImage: `url(${image})` }} className="bgImage" />
+    </div>
+  );
 }
+
+export default Button;
