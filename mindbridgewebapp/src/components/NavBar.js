50c4b01049b5e1f93a0b053670af5be187e0f657
Ryan Sadeghi-Javid <javidryan7@gmail.com>
2024-03-09 23:56:10 -0500

    Dropdown Menu

--- mindbridgewebapp/src/components/NavBar.js
+++ mindbridgewebapp/src/components/NavBar.js
@@ -5,16 +5,19 @@ import profile from "../assets/profile1.png";
 import "../styles/NavBar.css";
 
 export default function NavBar() {
-	return (	
-		<div className = 'navbar justify-between'>
-			<div className = 'white head-text justify-front'>
-				MindBridge
+	return (
+	  <div className="navbar justify-between">
+		<div className="white head-text justify-front">MindBridge</div>
+		<div className="dropdown">
+		  <NavLink className="justify-end" to={"/Profile"}>
+			<img src={profile} className="image" alt="Profile" />
+			<div className="dropdown-content">
+			  <NavLink to="/profile/settings">Settings</NavLink>
+			  <NavLink to="/profile/logout">Logout</NavLink>
 			</div>
-			<NavLink 
-				className = 'justify-end'
-				to={"/Profile"} >
-				<img src={profile} className= 'image'/>
-			</NavLink>
+		  </NavLink>
 		</div>
+	  </div>
 	);
-}
+  }
+  
